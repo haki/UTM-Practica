0ebbd417d1168ee6f2bcb06e84a39721a275efe9
Hakan Meral <hakanmeral99@gmail.com>
2020-04-27 14:41:58 +0300

    first-commit

---
+++ src/test/java/md/utm/universty/UniverstyNowApplicationTests.java
@@ -0,0 +1,13 @@
+package md.utm.universty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverstyNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
